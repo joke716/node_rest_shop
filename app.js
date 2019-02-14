0010b609e6e4ee32e6666e72242f0f7534a87eb1
Teddy <joke79@hanyang.ac.kr>
2019-02-14 14:08:18 +0900

    added request body parsing and cors config

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 
 // app.use((req, res, next) => {
@@ -10,18 +11,35 @@ const morgan = require('morgan');
 //     });
 // });
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
 
-app.use(morgan('dev'));
-
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use((req, res, next) => {
-    const error = new Error('Not found');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
     error.status = 404;
     next(error);
 })
